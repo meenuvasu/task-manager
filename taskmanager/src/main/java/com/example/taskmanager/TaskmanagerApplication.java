2a4d0fb752898f2195c796650f75d9ecb0c34935
MANASHA M <23suca42@tcarts.in>
2025-12-26 09:58:51 +0530

    Update TaskmanagerApplication.java

--- taskmanager/src/main/java/com/example/taskmanager/TaskmanagerApplication.java
+++ taskmanager/src/main/java/com/example/taskmanager/TaskmanagerApplication.java
@@ -9,5 +9,6 @@ public class TaskmanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
 	}
-
+//this is for demo 
+	//done by manashamin
 }
